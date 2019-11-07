02f92ca1020437e1eb4517c6697370fe27134b17
ZhaoShaofeng <bem.zhao@foxmail.com>
2019-11-07 23:29:40 +0800

    列表循环、事件处理、setState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+    	inputValue: '',
+    	list: ["111", "222"]
+    };
+
+    // 为了在回调中使用 `this`，这个绑定是必不可少的
+    this.addMenu = this.addMenu.bind(this);
+    this.inputChange = this.inputChange.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.inputChange} name="" />
+        <button type="button" onClick={this.addMenu}>提交</button>
+        <ul>
+        	{
+        		this.state.list.map((item, index) => {
+        			return <li key={index}>{item}</li>
+	        	})
+        	}
+        </ul>
+      </div>
+    );
+  }
+
+  addMenu () {
+  	// this.setState({
+  	// 	list: [...this.state.list, this.state.inputValue]
+  	// });
+
+  	this.setState((state) => {
+  		return {list: state.list.concat(this.state.inputValue)};
+  	});
+
+	}
+
+	inputChange (e) {
+  	this.setState({
+  		inputValue: e.target.value
+  	});
+	}
 }
 
 export default App;
---
+++ src/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Menu extends React.Component {
+	render () {
+		return {
+			<li>111111</li>
+		}
+	}
+}
+
+export default Menu
