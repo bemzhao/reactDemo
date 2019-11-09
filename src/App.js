76a12d8dc821abab5cdb55160da496c83825f935
ZhaoShaofeng <bem.zhao@foxmail.com>
2019-11-09 23:23:33 +0800

    组件拆分 父子组件传值、props ref使用

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Menu from './Menu';
 
 class App extends React.Component {
 	constructor(props) {
@@ -11,17 +12,20 @@ class App extends React.Component {
     // 为了在回调中使用 `this`，这个绑定是必不可少的
     this.addMenu = this.addMenu.bind(this);
     this.inputChange = this.inputChange.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
   render() {
     return (
       <div>
-        <input type="text" onChange={this.inputChange} name="" />
+        <input type="text" value={this.state.inputValue} onChange={this.inputChange} ref={(input) => {this.input = input}} name="" />
         <button type="button" onClick={this.addMenu}>提交</button>
         <ul>
         	{
         		this.state.list.map((item, index) => {
-        			return <li key={index}>{item}</li>
+        			return (
+        					<Menu key={index} content={item} index={index} deleteItem={this.deleteItem}/>
+        			)
 	        	})
         	}
         </ul>
@@ -33,17 +37,35 @@ class App extends React.Component {
   	// this.setState({
   	// 	list: [...this.state.list, this.state.inputValue]
   	// });
+  	if (this.state.inputValue === '') {
+  		alert("不能为空");
+  		return false;
+  	}
 
   	this.setState((state) => {
-  		return {list: state.list.concat(this.state.inputValue)};
+  		return {
+  			inputValue: '',
+  			list: state.list.concat(this.state.inputValue)
+  		};
   	});
 
 	}
 
 	inputChange (e) {
   	this.setState({
-  		inputValue: e.target.value
+  		inputValue: this.input.value
   	});
+	}
+
+	deleteItem (index) {
+		// this.setState({
+		// 	state.list.splice(index,1);
+		// })
+		let list = this.state.list;
+		list.splice(index, 1);
+		this.setState({
+			list: list
+		})
 	}
 }
 
--- src/Menu.js
+++ src/Menu.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 class Menu extends React.Component {
 	render () {
-		return {
-			<li>111111</li>
-		}
+		return (
+			<li onClick={this.handleClick}>{this.props.content}</li>
+		)
+	}
+
+	handleClick = () => {
+		this.props.deleteItem(this.props.index)
 	}
 }
 
-export default Menu
+export default Menu;
