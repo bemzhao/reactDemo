cf58a7162a6110b14261818b9c0330c51ce2f8be
bemzhao <bem.zhao@foxmail.com>
2021-11-01 16:23:35 +0800

    数据模拟更换为 fastMock

--- src/App.js
+++ src/App.js
@@ -1,84 +1,89 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import Item from './item';
-import Bar from './bar';
+import React, { Component } from "react";
+import axios from "axios";
+import Item from "./item";
+import Bar from "./bar";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			inputValue: '',
-			lists: ["111", "222"]
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      lists: ["111", "222"]
+    };
+  }
 
-	componentDidMount() {
-		axios.post('http://rap2api.taobao.org/app/mock/236179/person').then((res) => {
-			this.setState({
-				lists: res.data.lists
-			})
-		}).catch((error => {
-			console.log(error)
-		}))
-	}
+  componentDidMount() {
+    axios
+      .get(
+        "https://www.fastmock.site/mock/5ccb57d10c61ef8c09adf2806eedab94/api/list"
+      )
+      .then((res) => {
+        console.log(res.data.data.list);
 
-	render() {
-		return (
-			<div>
-			  <Bar 
-			    inputValue={this.state.inputValue} 
-			    onChange={this.inputChange} 
-			    onKeyUp={this.keycode} 
-			    addMenu={this.addMenu}
-			  />
-			  <ul>
-			    {
-			      this.state.lists.map((item, index) => {
-			        return (
-			          <Item 
-						key={index} 
-						content={item.name} 
-						index={index} 
-						handleClick={this.handleDelete.bind(this, index)}
-					  />
-			        )
-			      })
-			    }
-			  </ul>
-			</div>
-		)
-	}
+        this.setState({
+          lists: res.data.data.list
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-	keycode = (e) => {
-		if (e.keyCode === 13) {
-			this.addMenu()
-		}
-	}
+  render() {
+    return (
+      <div>
+        <Bar
+          inputValue={this.state.inputValue}
+          onChange={this.inputChange}
+          onKeyUp={this.keycode}
+          addMenu={this.addMenu}
+        />
+        <ul>
+          {this.state.lists.map((item, index) => {
+            return (
+              <Item
+                key={index}
+                data={item}
+                index={index}
+                handleClick={this.handleDelete.bind(this, index)}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 
-	inputChange = (e) => {
-		this.setState({
-			inputValue: e.target.value
-		})
-	}
-	addMenu = () => {
-		if (this.state.inputValue.trim() === '') {
-			alert("不能为空");
-			return false;
-		}
-		let lists = this.state.lists;
-		lists.push({name: this.state.inputValue});
-		this.setState({
-			inputValue: "",
-			lists: lists
-		})
-	}
-	handleDelete = (index) => {
-		let lists = this.state.lists;
-		lists.splice(index, 1);
-		this.setState({
-			lists: lists
-		})
-	}
+  keycode = (e) => {
+    if (e.keyCode === 13) {
+      this.addMenu();
+    }
+  };
+
+  inputChange = (e) => {
+    this.setState({
+      inputValue: e.target.value
+    });
+  };
+  addMenu = () => {
+    if (this.state.inputValue.trim() === "") {
+      alert("不能为空");
+      return false;
+    }
+    let lists = this.state.lists;
+    lists.push({ name: this.state.inputValue });
+    this.setState({
+      inputValue: "",
+      lists: lists
+    });
+  };
+  handleDelete = (index) => {
+    let lists = this.state.lists;
+    lists.splice(index, 1);
+    this.setState({
+      lists: lists
+    });
+  };
 }
 
 export default App;
--- src/item.js
+++ src/item.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 
-class Item extends React.Component{
-	render () {
-		return (
-			<li onClick={this.handleClick}>{this.props.content}</li>
-		)
-	}
+class Item extends React.Component {
+  render() {
+    const { props } = this;
+    return (
+      <li onClick={this.handleClick}>
+        <span>{props.data.name}</span>
+        <img src={props.data.image} />
+      </li>
+    );
+  }
 
-	handleClick = () => {
-		this.props.handleClick(this.props.index)
-	}
+  handleClick = () => {
+    this.props.handleClick(this.props.index);
+  };
 }
 export default Item;
